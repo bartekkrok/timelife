247b722e2f554254f67256412f8dd301e5f5f153
bartekkrok <bartlomiej.krok@b-connected.io>
2025-03-09 21:30:08 +0100

    Memory item initial styles

--- components/MemoryItem.tsx
+++ components/MemoryItem.tsx
@@ -1,10 +1,10 @@
-import { Image, Text, View } from 'react-native';
+import { Image, Text, View, StyleSheet } from 'react-native';
 import { MemoryShortDetails } from '@/types/memory';
 
 const MemoryItem = (props: MemoryShortDetails) => {
   return (
-    <View>
-      <Image source={require('@/assets/images/partial-react-logo.png')} />
+    <View style={styles.container}>
+      <Image source={require('@/assets/images/partial-react-logo.png')} style={styles.image} />
       <View>
         <Text>{props.title}</Text>
         <Text>{props.date}</Text>
@@ -14,3 +14,16 @@ const MemoryItem = (props: MemoryShortDetails) => {
 };
 
 export default MemoryItem;
+
+const styles = StyleSheet.create({
+  image: {
+    width: 40,
+    height: 40,
+    marginRight: 8,
+  },
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 8,
+  },
+});
